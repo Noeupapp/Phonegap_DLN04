1e859c4ce6225d8a0f5663ecc1bf4bcebb676558
quentin <quentin@noeupapp.com>
2017-07-11 09:14:32 +0200

    Device motion with or without watch

--- js/index.js
+++ js/index.js
@@ -1,4 +1,38 @@
  $(document).ready(function(){
-    // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
-    $('.modal').modal();
+
+        function onSuccess(acceleration) {
+
+          var newX = Math.floor((acceleration.x+10)*255/20);
+          var newY = Math.floor((acceleration.y+10)*255/20);
+          var newZ = Math.floor((acceleration.z+10)*255/20);
+
+            $("body").css("background-color","rgb("+newX+","+newY+","+newZ+")")
+        }
+
+        function onError() {
+            alert('onError!');
+        }
+
+        setTimeout(function(){
+          navigator.accelerometer.watchAcceleration(onSuccess, onError,{frequency:500});
+        },4000)
+
+
+
+        $("#getAccel").on("click",function(){
+
+            function onSuccessClick(acceleration) {
+                $("#resultAccel").html("x:"+acceleration.x+"<br>"+
+                  "y : "+acceleration.y+"<br>"+
+                  "z : "+acceleration.z);
+            }
+
+            function onErrorClick() {
+                alert('onError!');
+            }
+
+            navigator.accelerometer.getCurrentAcceleration(onSuccessClick, onErrorClick);
+
+        })
+
   });
